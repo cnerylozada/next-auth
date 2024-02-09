4c7c794ec06964713333d908afb600ba3e807ba5
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2024-02-09 17:26:23 -0500

    feat: provider was created and user session data was shown

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,24 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export const Navbar = () => {
+  const { data, status } = useSession();
   return (
     <div className="p-4 flex space-x-4 bg-yellow-50">
       <div>Navbar</div>
-      <div>
+      <div className="flex space-x-4">
         <div>
-          <Link href={"/api/auth/signin"}>Login</Link>
+          {status === "unauthenticated" && (
+            <Link href={"/api/auth/signin"}>Login</Link>
+          )}
+          {status === "authenticated" && <>{data.user?.name}</>}
         </div>
+        {status === "authenticated" && (
+          <div>
+            <button onClick={() => signOut()}>Sign out</button>
+          </div>
+        )}
       </div>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./components/Navbar";
+import { Provider } from "./components/auth/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <div>{children}</div>
+        <Provider>
+          <Navbar />
+          <div>{children}</div>
+        </Provider>
       </body>
     </html>
   );
