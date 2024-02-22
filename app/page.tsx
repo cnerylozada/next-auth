ebda1f8d8f9abd248afcbfc6ac266e0965a58324
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2024-02-22 09:33:09 -0500

    feat: accessing session on the server

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
     <div className="">
       <div>Home page</div>
+      <div>Hello {session && <span>{session?.user?.name}</span>}</div>
     </div>
   );
 }
